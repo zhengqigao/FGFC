acbdcedde5d1f94281e48ecb006f95bfb27019d4
zhengqigao <zhengqi@mit.edu>
2023-08-21 10:49:15 -0500

    initial online version

--- parse.py
+++ parse.py
@@ -1,67 +1,64 @@
-import os
-import glob
 import yaml
 
-def parse_yaml_file(file_path):
-    with open(file_path, "r") as file:
-        data = yaml.safe_load(file)
-    return data
+# Read the YAML data from the external file
+with open("collection.yaml", "r") as yaml_file:
+    yaml_data = yaml.safe_load(yaml_file)
 
-def generate_html_by_category(profiles, categories):
-    html_content = """<!DOCTYPE html>
+# Sort the data alphabetically by name
+sorted_data = sorted(yaml_data.values(), key=lambda person: f"{person['LastName']} {person['FirstName']}")
+
+
+# Generate HTML content
+html_content = f"""
 <html>
 <head>
-    <title>Profiles by Category</title>
-    <!-- Add Bootstrap CSS link -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<title>Fudan-Graduated Faculty Collection List</title>
+<style>
+h1 {{
+    font-size: 20px;
+}}
+</style>
 </head>
 <body>
-    <div class="container">
-    """
 
-    for category in categories:
-        html_content += f'<h2>{category} Category</h2>'
-        html_content += '<div class="row">'
-        
-        category_profiles = [profile for profile in profiles if profile["Mandatory"]["Category"] == category]
-        for profile in category_profiles:
-            html_content += '<div class="col-md-6">'
-            html_content += '<div class="card my-4">'
-            html_content += f'<div class="card-body">'
-            html_content += f'<h4 class="card-title">{profile["Mandatory"]["First Name"]} {profile["Mandatory"]["Last Name"]}</h4>'
-            html_content += f'<p class="card-text">School: {profile["Mandatory"]["Full School Name"]}</p>'
-            html_content += f'<p class="card-text">Email: {profile["Mandatory"]["Verify Email"]}</p>'
-            html_content += f'<p class="card-text">Short Bio: {profile["Optional"].get("Short Bio", "N/A")}</p>'
-            html_content += '</div></div></div>'
-        
-        html_content += '</div>'
-    
-    html_content += """    </div>
-    <!-- Add Bootstrap JS and jQuery links -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<h1>Introduction</h1>
+<p>This list functions as a bridge, linking both older and newer generations of Fudan students engaged in research. While Fudan University unquestionably boasts prestige, we must also recognize its shortcomings when compared to Tsinghua and Peking universities. Just contemplate the discrepancy in the number of Fudan graduates working in top universities worldwide compared to those from Tsinghua. Through this list, our aspiration is for forthcoming generations of Fudan students seeking research opportunities to establish meaningful connections with their senior Fudan alumni. We believe that professors who have graduated from Fudan would be inclined to provide assistance and offer internship opportunities upon receiving an email from a current Fudan student with a high GPA.
+</p>
+
+<h1>FGFC List</h1>
+<p> Each entry is organized in the following format: Name (University, Category) — Research Keywords. Please note that [Category] may not necessarily correspond directly to the individual's department or school name, but rather reflects their research interests. The following list is arranged in alphabetical order.
+</p>
+<ol>
+"""
+
+for key, person in yaml_data.items():
+    name = f"{person['FirstName']} {person['LastName']}"
+    affiliation = person['Affiliation']
+    category = person['Category']
+    webpage = person['Webpage']
+    research_key = person.get('ResearchKey', '')  # Get the ResearchKey if present, or use an empty string
+
+    item = f"<li><a href='{webpage}'>{name}</a> ({affiliation}, {category}) --- {research_key}</li>"
+    html_content += item
+
+html_content += """
+</ol>
+
+<h1>Contributors</h1>
+<p>This list is contributed by: <a href="https://zhengqigao.github.io/">Zhengqi Gao</a></p>
+
+<h1>Feedback</h1>
+<p>
+If you come across any missing professor information, kindly proceed to create a pull request on <a href="https://github.com/zhengqigao/FGFC">our GitHub repo</a>, or feel free to reach out to us via email at <a href="mailto:fgfc23.start@gmail.com">fgfc23.start@gmail.com</a>. We welcome any suggestions or comments.
+</p>
+
+
 </body>
 </html>
 """
-    return html_content
 
-def main():
-    directory_path = "./list/"  # Replace with the actual directory path
-    output_html_path = "./html/main.html"  # Change this to your desired output HTML file name
-    categories = ['EE', 'CS', 'Math', 'Stat']  # Pre-defined categories
+# Write the HTML content to an "index.html" file
+with open("index.html", "w") as html_file:
+    html_file.write(html_content)
 
-    yaml_files = glob.glob(os.path.join(directory_path, "*.yaml"))
-    profiles = []
-
-    for yaml_file in yaml_files:
-        profile_data = parse_yaml_file(yaml_file)
-        profiles.append(profile_data)
-
-    html_content = generate_html_by_category(profiles, categories)
-
-    with open(output_html_path, "w") as output_file:
-        output_file.write(html_content)
-
-if __name__ == "__main__":
-    main()
+print("HTML file generated successfully.")
